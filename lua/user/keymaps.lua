36f6841555ccb0514fd69d9fd876fffbc144d53c
zhangzhenghan <zhangzh@4dstc.com>
2022-10-09 12:36:19 +0800

    add some plugins

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -95,3 +95,26 @@ keymap("n", "<leader>bL", "<cmd>BufferLineSortByExtension<cr>", opts)
 keymap("t", "<leader>rr", "<cmd>RnvimrResize<cr>", opts)
 keymap("n", "<leader>rg", "<cmd>RnvimrToggle<cr>", opts)
 keymap("t", "<leader>rg", "<cmd>RnvimrToggle<cr>", opts)
+
+-- hop
+keymap("n", "<leader><leader>w", "<cmd>HopWord<cr>", opts)
+
+-- nvim spectre
+keymap("n", "<leader>S", "<cmd>lua require('spectre').open()<CR>", opts)
+
+-- alpha
+keymap("n", "<leader>;", "<cmd>Alpha<CR>", opts)
+
+-- auto-save
+keymap("n", "<leader>at", "<cmd>ASToggle<CR>", opts)
+
+-- persistence
+keymap("n", "<leader>pl", "<cmd>lua require('persistence').load()<cr>", opts)
+
+-- trouble
+keymap("n", "<leader>xx", "<cmd>TroubleToggle<cr>", opts)
+keymap("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>", opts)
+keymap("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>", opts)
+keymap("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>", opts)
+keymap("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>", opts)
+keymap("n", "gR", "<cmd>TroubleToggle lsp_references<cr>", opts)
