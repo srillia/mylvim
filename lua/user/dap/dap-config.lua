5ce60502bdb38a7052b34820bb900cef7b0253b9
zhangzhenghan <zhangzh@4dstc.com>
2022-10-11 21:40:07 +0800

    debug in new tab

--- lua/user/dap/dap-config.lua
+++ lua/user/dap/dap-config.lua
@@ -32,7 +32,7 @@ dapui.setup({
 	tray = {
 		elements = {
 			"repl",
-      "console",
+			"console",
 		},
 		position = "bottom",
 	},
@@ -40,14 +40,56 @@ dapui.setup({
 
 vim.fn.sign_define("DapBreakpoint", { text = "", texthl = "DiagnosticSignError", linehl = "", numhl = "" })
 
-dap.listeners.after.event_initialized["dapui_config"] = function()
-	dapui.open()
+-- dap.listeners.after.event_initialized["dapui_config"] = function()
+-- 	dapui.open()
+-- end
+--
+-- dap.listeners.before.event_terminated["dapui_config"] = function()
+-- 	dapui.close()
+-- end
+--
+-- dap.listeners.before.event_exited["dapui_config"] = function()
+-- 	dapui.close()
+-- end
+
+local debug_win = nil
+local debug_tab = nil
+local debug_tabnr = nil
+
+local function open_in_tab()
+  if debug_win and vim.api.nvim_win_is_valid(debug_win) then
+    vim.api.nvim_set_current_win(debug_win)
+    return
+  end
+
+  vim.cmd('tabedit %')
+  debug_win = vim.fn.win_getid()
+  debug_tab = vim.api.nvim_win_get_tabpage(debug_win)
+  debug_tabnr = vim.api.nvim_tabpage_get_number(debug_tab)
+
+  dapui.open()
 end
 
-dap.listeners.before.event_terminated["dapui_config"] = function()
-	dapui.close()
+local function close_tab()
+  dapui.close()
+
+  if debug_tab and vim.api.nvim_tabpage_is_valid(debug_tab) then
+    vim.api.nvim_exec('tabclose ' .. debug_tabnr, false)
+  end
+
+  debug_win = nil
+  debug_tab = nil
+  debug_tabnr = nil
 end
 
-dap.listeners.before.event_exited["dapui_config"] = function()
-	dapui.close()
+-- Attach DAP UI to DAP events
+dap.listeners.after.event_initialized['dapui_config'] = function()
+  open_in_tab()
 end
+dap.listeners.before.event_terminated['dapui_config'] = function()
+  close_tab()
+end
+dap.listeners.before.event_exited['dapui_config'] = function()
+  close_tab()
+end
+
