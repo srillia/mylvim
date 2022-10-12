1ff87dba237a19496c5744cfae742056675dffd3
zhangzhenghan <zhangzh@4dstc.com>
2022-10-12 12:13:16 +0800

    add schemastore

--- lua/user/lsp/settings/jsonls.lua
+++ lua/user/lsp/settings/jsonls.lua
@@ -1,7 +1,9 @@
 return {
-  settings = {
-    json = {
-      -- schemas = require'nlspsettings.loaders.json'.get_default_schemas()
-    },
-  },
+	settings = {
+		json = {
+			schemas = require("schemastore").json.schemas(),
+			validate = { enable = true },
+			-- schemas = require'nlspsettings.loaders.json'.get_default_schemas()
+		},
+	},
 }
