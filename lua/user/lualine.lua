d6ff7828fb827133dec907051083ec774bd20368
zhangzhenghan <zhangzh@4dstc.com>
2022-10-13 14:58:58 +0800

    lualine add lsp info

--- lua/user/lualine.lua
+++ lua/user/lualine.lua
@@ -1,58 +1,70 @@
 local status_ok, lualine = pcall(require, "lualine")
 if not status_ok then
-  return
+	return
 end
 
 local hide_in_width = function()
-  return vim.fn.winwidth(0) > 80
+	return vim.fn.winwidth(0) > 80
 end
 
 local diagnostics = {
-  "diagnostics",
-  sources = { "nvim_diagnostic" },
-  sections = { "error", "warn" },
-  symbols = { error = " ", warn = " " },
-  colored = false,
-  always_visible = true,
+	"diagnostics",
+	sources = { "nvim_diagnostic" },
+	sections = { "error", "warn" },
+	symbols = { error = " ", warn = " " },
+	colored = false,
+	always_visible = true,
 }
 
 local diff = {
-  "diff",
-  colored = false,
-  symbols = { added = "", modified = "", removed = "" }, -- changes diff symbols
-  cond = hide_in_width,
+	"diff",
+	colored = false,
+	symbols = { added = "", modified = "", removed = "" }, -- changes diff symbols
+	cond = hide_in_width,
 }
 
 local filetype = {
-  "filetype",
-  icons_enabled = false,
+	"filetype",
+	icons_enabled = true,
 }
 
 local location = {
-  "location",
-  padding = 0,
+	"location",
+	padding = 0,
 }
 
 local spaces = function()
-  return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
+	return "spaces: " .. vim.api.nvim_buf_get_option(0, "shiftwidth")
 end
 
-lualine.setup {
-  options = {
-    globalstatus = true,
-    icons_enabled = true,
-    theme = "auto",
-    component_separators = { left = "", right = "" },
-    section_separators = { left = "", right = "" },
-    disabled_filetypes = { "alpha", "dashboard" },
-    always_divide_middle = true,
-  },
-  sections = {
-    lualine_a = { "mode" },
-    lualine_b = {"branch"},
-    lualine_c = { diagnostics },
-    lualine_x = { diff, spaces, "encoding", filetype },
-    lualine_y = { location },
-    lualine_z = { "progress" },
-  },
-}
+local lsp = function()
+	local buf_clients = vim.lsp.buf_get_clients()
+	if next(buf_clients) ~= nil then
+		for _, client in pairs(buf_clients) do
+			if client.name ~= "null-ls" then
+				return client.name
+			end
+		end
+	end
+  return ""
+end
+
+lualine.setup({
+	options = {
+		globalstatus = true,
+		icons_enabled = true,
+		theme = "auto",
+		component_separators = { left = "", right = "" },
+		section_separators = { left = "", right = "" },
+		disabled_filetypes = { "alpha", "dashboard" },
+		always_divide_middle = true,
+	},
+	sections = {
+		lualine_a = { "mode" },
+		lualine_b = { "branch" },
+		lualine_c = { diagnostics },
+		lualine_x = { diff, spaces, "encoding", lsp, filetype },
+		lualine_y = { location },
+		lualine_z = { "progress" },
+	},
+})
