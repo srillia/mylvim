6892122abb29f340618599ee8f664ee8840912e7
zhangzhenghan <zhangzh@4dstc.com>
2022-10-17 10:50:16 +0800

    fix warn

--- lua/user/lsp/handlers.lua
+++ lua/user/lsp/handlers.lua
@@ -17,7 +17,7 @@ end
 
 M.capabilities = vim.lsp.protocol.make_client_capabilities()
 M.capabilities.textDocument.completion.completionItem.snippetSupport = true
-M.capabilities = cmp_nvim_lsp.update_capabilities(M.capabilities)
+M.capabilities = cmp_nvim_lsp.default_capabilities(M.capabilities)
 
 M.setup = function()
 	local signs = {
@@ -107,10 +107,6 @@ M.on_attach = function(client, bufnr)
 		client.server_capabilities.document_formatting = false
 		client.server_capabilities.textDocument.completion.completionItem.snippetSupport = false
 	end
-
-	M.capabilities = vim.lsp.protocol.make_client_capabilities()
-	M.capabilities.textDocument.completion.completionItem.snippetSupport = true
-	M.capabilities = cmp_nvim_lsp.update_capabilities(M.capabilities)
 end
 
 return M
