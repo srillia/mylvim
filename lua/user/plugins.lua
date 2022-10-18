6dcafe3e7419858b715a89d107cb17fbef1708c9
zhangzhenghan <zhangzh@4dstc.com>
2022-10-18 15:27:24 +0800

    add autotags

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -56,6 +56,7 @@ return packer.startup(function(use)
 	use({ "hrsh7th/cmp-nvim-lsp" })
 	use({ "hrsh7th/cmp-nvim-lua" })
 	use({ "windwp/nvim-autopairs" }) -- Autopairs, integrates with both cmp and treesitter
+	use({ "windwp/nvim-ts-autotag" })
 	-- snippets
 	use({ "L3MON4D3/LuaSnip" }) --snippet engine
 	use({ "rafamadriz/friendly-snippets" }) -- a bunch of snippets to use
