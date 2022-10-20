17a827641cb918bdb0e289052df24a52aea07c12
zhangzhenghan <zhangzh@4dstc.com>
2022-10-20 16:55:03 +0800

    delete line

--- lua/user/ui/treesitter.lua
+++ lua/user/ui/treesitter.lua
@@ -21,7 +21,6 @@ configs.setup({
 			local max_filesize = 50 * 1024 -- 100 KB
 			local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
 			if ok and stats and stats.size > max_filesize then
-        require("user.utils").notify("return true")
 				return true
 			end
 		end,
